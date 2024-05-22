184ed8b8e2b096ad0f4f354aa0a6b2e64aff02b9
Mohammad <msaad@e2abs.com>
2024-05-22 18:29:00 +0300

    updated the structure

--- genpage.ps1
+++ genpage.ps1
@@ -1,19 +1,20 @@
 param (
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string]$category,
 
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string]$pageName
 )
 
-# Singularize pageName
+# Singularize
+$singularCategory = $category.TrimEnd("s")
 $singularPageName = $pageName.TrimEnd("s")
 
 # Generate Angular components, interfaces, and services
-ng generate component --standalone "portal/$category/$category-$pageName"
+# ng generate component --standalone "portal/$category/$category-$pageName"
 ng generate interface "shared/interfaces/$category/$singularPageName"
 ng generate service "shared/services/$category/$singularPageName"
-ng generate component --standalone "portal/$category/$category-$pageName/$pageName-details"
-ng generate component --standalone "portal/$category/$category-$pageName/new-$pageName"
+ng generate component --standalone "portal/$category/$category-$pageName/$singularPageName-$singularCategory-details"
+ng generate component --standalone "portal/$category/$category-$pageName/new-$singularPageName-$singularCategory"
 
 Write-Output "Components, interface, and service for $category/$pageName have been generated successfully."
